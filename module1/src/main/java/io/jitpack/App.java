cfdfd81f47f1f03e3f66ca40f937994c222ca400
User <User@DESKTOP-26CJV3T.mshome.net>
2020-11-01 16:53:50 +0800

    測試一下 學習GIT

--- module1/src/main/java/io/jitpack/App.java
+++ module1/src/main/java/io/jitpack/App.java
@@ -4,12 +4,11 @@ package io.jitpack;
  * Hello world!
  *
  */
-public class App 
-{
-    public static final String GREETING = "Hello World!";
-    
-    public static void main( String[] args )
-    {
-        System.out.println(GREETING);
-    }
+public class App {
+	public static final String GREETING = "Hello World!";
+
+	public static void main(String[] args) {
+		System.out.println(GREETING);
+		System.out.println("fork commit 測試一下 讓我努力一下 好ㄇ");
+	}
 }
